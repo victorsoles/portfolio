511585f1b4654f09d2c2a504bafe8a3d836da069
Victor Soles <victorsoles09@gmail.com>
2025-01-22 23:21:02 -0300

    Inicializando projeto

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <div class="bg-blue-500 text-center">Oi</div>
+  </div>
+</template>
+
+<style scoped>
+</style>
